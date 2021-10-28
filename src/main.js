790f90335dd4b900dcce3e8b7d15baf2aecf7b39
tilda <iamtheworst@programmer.net>
2021-10-28 13:51:58 -0500

    add vue-router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import store from './store'
+import router from './router'
 
 Vue.use(Buefy)
 
@@ -12,5 +13,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   store,
+  router,
   render: (h) => h(App)
 }).$mount("#app");
