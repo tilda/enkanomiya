776da698c4c45956b61119d78ab289c55f3dc09c
tilda <iamtheworst@programmer.net>
2021-10-28 18:47:30 -0500

    fix mistake in home page (it's not a dropdown)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
                 <h1>Welcome to enkanomiya!</h1>
                 <p>
                     This website contains various tools to help you in your <i>Genshin Impact</i> experience.<br/>
-                    Use the dropdown at the top to switch between the tools offered.<br/>
+                    Use the navigation at the top to switch between the tools offered.<br/>
                     For more information, see <router-link to="/about">the about page</router-link>.
                 </p>
             </div>
