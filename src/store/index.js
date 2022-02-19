dbf86372f0390e88e5f8bc2d01b54e6c132aba29
tilda <iamtheworst@programmer.net>
2022-02-19 11:02:08 -0600

    finally start actually using vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,22 +3,30 @@ import Vuex from 'vuex'
 import VuexPersist from 'vuex-persist'
 
 const persist = new VuexPersist({
-  key: 'state',
-  storage: window.localStorage
+    key: 'state',
+    storage: window.localStorage
 })
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-  plugins: [
-    persist.plugin
-  ]
+    state: {
+        learned_recipes: [],
+        mastered_recipes: [],
+        pinned_recipes: []
+    },
+    mutations: {
+        markRecipeCompleted(state, recipeID) {
+            state.completed_recipes += recipeID
+        },
+        resetRecipeList(state) {
+            state.learned_recipes = []
+            state.mastered_recipes = []
+        }
+    },
+    actions: {},
+    modules: {},
+    plugins: [
+        persist.plugin
+    ]
 })
